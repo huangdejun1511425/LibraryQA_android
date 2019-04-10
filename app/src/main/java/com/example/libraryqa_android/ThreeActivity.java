ddc7849f8e0192b4ced3e607090da507d84b5d18
huangdejun1511425 <1968486175@qq.com>
2019-04-10 15:17:22 +0800

    add UI design

--- app/src/main/java/com/example/libraryqa_android/ThreeActivity.java
+++ app/src/main/java/com/example/libraryqa_android/ThreeActivity.java
@@ -3,37 +3,40 @@ package com.example.libraryqa_android;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.TextView;
+import android.widget.ImageView;
+
+import com.skyfishjy.library.RippleBackground;
+
+import pl.droidsonroids.gif.GifImageView;
 
 
 public class ThreeActivity extends Activity {
-    private TextView textView1;
+    private GifImageView textView1;
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         //全屏显示
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|View.SYSTEM_UI_FLAG_IMMERSIVE;
+        getWindow().setAttributes(params);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        if(NavUtils.getParentActivityName(ThreeActivity.this)!=null) {
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
         setContentView(R.layout.three_activity);
-        //view.setKeepScreenOn(true);//设置常亮
-        //设置返回键
-        //以下代码写在onCreate（）方法当中
-        textView1 = (TextView) findViewById(R.id.sound_help);
-        textView1.setOnClickListener(new View.OnClickListener() {
-
+        final RippleBackground rippleBackground=(RippleBackground)findViewById(R.id.content);
+        rippleBackground.startRippleAnimation();
+        ImageView imageView=(ImageView)findViewById(R.id.centerImage);
+        imageView.setOnClickListener(new View.OnClickListener() {
             @Override
-
-            public void onClick(View arg0) {
-
-
-                Intent intent = new Intent(ThreeActivity.this, MainActivity.class);
-
+            public void onClick(View view) {
+                Intent intent = new Intent(ThreeActivity.this,MainActivity.class);
                 startActivity(intent);
-
             }
-
         });
     }
 }
