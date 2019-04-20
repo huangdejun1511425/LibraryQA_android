e4b0f8a2ed9c12f865a6d3c1da98a2170c3bc257
huangdejun1511425 <30589109+huangdejun1511425@users.noreply.github.com>
2019-04-20 22:55:49 +0800

    Update HttpPost.java
    add some annotations

--- app/src/main/java/com/example/libraryqa_android/HttpPost.java
+++ app/src/main/java/com/example/libraryqa_android/HttpPost.java
@@ -16,14 +16,15 @@ public class HttpPost {
     String gAsw;
 
     public void hPost(String ask, final Info info) {
+        //传递json格式
         MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
         String requestBody = "{\"question\": \" " + ask + "\"}";
-        System.out.println("33333" + ask);
         Request request = new Request.Builder()
                 .url("http://39.108.80.74:8888?target=all")
                 .post(RequestBody.create(mediaType, requestBody))
                 .build();
         OkHttpClient okHttpClient = new OkHttpClient();
+        //回调
         okHttpClient.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
@@ -35,9 +36,6 @@ public class HttpPost {
                 Gson gson = new Gson();
                 Info result = gson.fromJson(response.body().string(), Info.class);
                 gAsw = result.graph_answer;
-                System.out.println("123456789" + result.getSearch_answer().get(0).getQuestion());
-                System.out.println("123456789" + result.getSearch_answer().get(1).getAnswer());
-                System.out.println("123456789" + result.getSearch_answer().get(2).getQuestion());
                 info.setGraph_answer(gAsw);
                 info.setSearch_answer(result.getSearch_answer());
             }
